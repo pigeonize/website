a3b3246d06c86d84fce70d4e2e44d736f735170e
pigeonize <lenoble.lewis@gmail.com>
2024-07-31 15:56:46 +0100

    Add files via upload

---
+++ about.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+    $(".links").hide();
+    $(".doubledagger").click(function(){
+        $(".links").toggle("slide", { direction: "right" }, 400); 
+      });
+    
+    });
+  
+  
+  
